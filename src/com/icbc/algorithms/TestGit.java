82b60a9f69f9376843ae247a1fb58c59bafff141
xielong7 <xielong7@jd.com>
2021-05-21 21:59:36 +0800

    TestGit
    TestGit

---
+++ src/com/icbc/algorithms/TestGit.java
@@ -0,0 +1,14 @@
+package com.icbc.algorithms;
+
+/**
+ * @Auther: xielong7
+ * @Date: 2021/5/21 21:15
+ * @Version: V1.0.0
+ * @Since: V1.0.0
+ * @Description:
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("学习GIT 测试用");
+    }
+}
